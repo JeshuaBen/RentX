214b2d0d3aeeb35ce33e09c1e7d93b0a4b9ac843
Jeshua Costa <jeshuacostafer@gmail.com>
2022-05-31 09:44:38 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+
+import { Home } from "./src/screens/Home";
+import theme from "./src/styles/theme";
+
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar />
+      <Home />
+    </ThemeProvider>
+  );
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Title } from "./style";
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
