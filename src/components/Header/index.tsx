9e5fe7a1b5414d4aefcb7331c2fdf25741d5d27b
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-04 22:57:28 -0300

    feat: stylized the header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
 
-import { Container } from "./style";
+import { Container, HeaderContent, TotalCars } from "./style";
 
 export const Header = () => {
-  return;
-  <Container>
-    <Logo />
-  </Container>;
+  return (
+    <Container>
+      <HeaderContent>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total de 12 carros</TotalCars>
+      </HeaderContent>
+    </Container>
+  );
 };
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,3 +1,4 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -5,4 +6,20 @@ export const Container = styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+
+  padding: 32px 24px 32px 16px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
