74ac4409253f7bacdf2a0bfe7857e95fcc4915f5
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-10 21:47:57 -0300

    feat: added the acessory component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+
+import { Accessory } from "../../components/Accessory";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
 
 import {
   Container,
@@ -8,6 +17,7 @@ import {
   CarImages,
   Content,
   Details,
+  Acessories,
   Description,
   Brand,
   Name,
@@ -43,6 +53,15 @@ export const CarDetails = () => {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Acessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Acessories>
 
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide
