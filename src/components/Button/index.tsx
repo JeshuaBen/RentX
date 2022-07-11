093ff4ff914b17af2540eb68fc3cebf71ed78251
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-10 23:17:59 -0300

    feat: finished the carDetails screen

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
 
 import { Container, Title } from "./style";
 
-interface Props {
+interface Props extends RectButtonProps {
   title: string;
   color?: string;
 }
--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,14 +1,20 @@
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+interface ButtonProps extends RectButtonProps {
+  color?: string;
+}
+
+export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
 
   padding: 19px;
   align-items: center;
   justify-content: center;
 
-  background-color: ${({ theme }) => theme.colors.main};
+  background-color: ${({ color, theme }) =>
+    color ? color : theme.colors.main};
 `;
 
 export const Title = styled.Text`
