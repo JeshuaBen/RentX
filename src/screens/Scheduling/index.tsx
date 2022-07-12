1c4eb96e02d75c099bfa5cf93f5dccaba1f96dc2
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-12 00:57:35 -0300

    feat: created the first structure of SchedulingScreen

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import { BackButton } from "../../components/BackButton";
 import { useTheme } from "styled-components";
-import { Container, Header, Title } from "./style";
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./style";
+
+import ArrowSvg from "../../assets/arrow.svg";
+import { Button } from "../../components/Button";
 
 export const Scheduling = () => {
   const theme = useTheme();
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Header>
         <BackButton onPress={() => {}} color={theme.colors.shape} />
 
@@ -15,7 +34,26 @@ export const Scheduling = () => {
           data de início e {"\n"}
           fim do aluguel
         </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
       </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 };
