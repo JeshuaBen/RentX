cd97594cb46373170922d20a5c1566da40078a46
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-19 23:08:05 -0300

    feat: finished the navigation

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StatusBar, useWindowDimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { ConfirmButton } from "../../components/ConfirmButton";
 import BackgroundGraySvg from "../../assets/logo_background_gray.svg";
@@ -13,9 +14,19 @@ import {
   Message,
   Footer,
 } from "./style";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../Routes/stack.routes";
+
+type SchedulingCompleteScreenProps = StackNavigationProp<RootStackParamList>;
 
 export const SchedulingComplete: React.FC = () => {
+  const navigation = useNavigation<SchedulingCompleteScreenProps>();
   const { width } = useWindowDimensions();
+
+  const handleConfirm = () => {
+    navigation.navigate("Home");
+  };
+
   return (
     <Container>
       <StatusBar
@@ -39,7 +50,7 @@ export const SchedulingComplete: React.FC = () => {
       </Content>
 
       <Footer>
-        <ConfirmButton title="OK" />
+        <ConfirmButton title="OK" onPress={handleConfirm} />
       </Footer>
     </Container>
   );
